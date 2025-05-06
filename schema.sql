1c86bf08392498e64640a37e93893b9c1751c5c1
Nayab-Sarfaraj <nayabsarfaraj@gmail.com>
2025-05-07 01:05:18 +0530

    fixed the sql file

--- schema.sql
+++ schema.sql
@@ -1,8 +1,7 @@
-CREATE TABLE notes(
-    id uuid primary key default gen_default_uuid(),
-    title varchar(100) not null,
-    content text not null,
-    important_points text[], 
-    user_id uuid references auth.users(id),npx supabase init
-    created_at timestamp with time zone default now()
-)
+create table notes (
+  id uuid primary key default gen_random_uuid(),
+  user_id uuid references auth.users(id),
+  title text not null,
+  content text not null,
+  created_at timestamp with time zone default now()
+);
